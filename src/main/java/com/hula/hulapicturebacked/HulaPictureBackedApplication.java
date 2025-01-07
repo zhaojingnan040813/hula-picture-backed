29e6cbebc84cb75af3f65b2e69f9a9d57546278f
hula <2026617199@qq.com>
2025-01-07 22:03:23 +0800

    初始化项目

---
+++ src/main/java/com/hula/hulapicturebacked/HulaPictureBackedApplication.java
@@ -0,0 +1,13 @@
+package com.hula.hulapicturebacked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HulaPictureBackedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HulaPictureBackedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hula/hulapicturebacked/HulaPictureBackedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hula.hulapicturebacked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HulaPictureBackedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
