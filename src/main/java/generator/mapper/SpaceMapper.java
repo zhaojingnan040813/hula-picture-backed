e77f44778fc20e9cbd9379d34cbf4dfda389f317
hula <2026617199@qq.com>
2025-03-05 16:40:50 +0800

    代码生成器生成space表的增删改查

---
+++ src/main/java/generator/mapper/SpaceMapper.java
@@ -0,0 +1,18 @@
+package generator.mapper;
+
+import generator.domain.Space;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Administrator
+* @description 针对表【space(空间)】的数据库操作Mapper
+* @createDate 2025-03-05 16:40:18
+* @Entity generator.domain.Space
+*/
+public interface SpaceMapper extends BaseMapper<Space> {
+
+}
+
+
+
+
